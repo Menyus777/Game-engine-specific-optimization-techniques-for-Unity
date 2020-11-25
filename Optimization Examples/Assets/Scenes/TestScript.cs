3a92bfddfb6a32b848b5ceb93cc902352120a332
Menyus <menyus777@gmail.com>
2020-11-25 21:10:25 +0100

    Adding performance test folder structure

---
+++ Optimization Examples/Assets/Scenes/TestScript.cs
@@ -0,0 +1,14 @@
+﻿// Copyright 2020 by RedFox Interactive - All Rights Reserved
+// Unauthorized copying of this file, is strictly prohibited
+
+using UnityEngine;
+
+namespace OptimizationExamples.Test
+{
+	public class TestScript : MonoBehaviour
+	{
+		void Update()
+		{
+		}
+	}
+}
---
+++ Optimization Examples/Assets/Tests/Performance/Scene 1/PerformanceTestExample.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PerformanceTestExample
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void PerformanceTestExampleSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator PerformanceTestExampleWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
