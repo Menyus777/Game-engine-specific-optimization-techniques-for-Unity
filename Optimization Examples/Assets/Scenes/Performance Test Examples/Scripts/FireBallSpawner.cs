2b7ce64baa9d235fb00856fd733ac988265c6a08
Menyus <menyus777@gmail.com>
2020-11-27 17:14:26 +0100

    Update with comment

--- Optimization Examples/Assets/Scenes/Performance Test Examples/Scripts/FireBallSpawner.cs
+++ Optimization Examples/Assets/Scenes/Performance Test Examples/Scripts/FireBallSpawner.cs
@@ -21,9 +21,15 @@ namespace OptimizationExamples.PerformanceTestExamples
 			for(int i = 0; i < count; i++)
 			{
 				Vector3 generatedPos = Random.onUnitSphere * 2.5f + _spawnPos.position;
-				Instantiate(_fireBallPrefab, generatedPos, Quaternion.identity);
-				//Destroy(Instantiate(_fireBallPrefab, generatedPos, Quaternion.identity), 2.0f);
+				//Instantiate(_fireBallPrefab, generatedPos, Quaternion.identity);
+				Destroy(Instantiate(_fireBallPrefab, generatedPos, Quaternion.identity), 3.0f);
 			}
 		}
+
+		// Ötletek:
+		// Az alap ami most van
+		// Destory mindenre egy fos megoldás jött egy új kolléga érkezik aki kijavítja a scriptet jóhiszeműen de hát szar lesz
+		// Destroy egy holder Go-ra
+		// Objectpooling
 	}
 }
