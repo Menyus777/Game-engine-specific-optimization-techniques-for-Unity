587f0be0de7a0a01818a6507bc149050bebcf1cf
Menyus-Asus_G <menyus777@gmai.com>
2020-11-28 23:46:23 +0100

    Added some example implementations

--- Optimization Examples/Assets/Scenes/Performance Test Examples/Scripts/FireBallSpawner.cs
+++ Optimization Examples/Assets/Scenes/Performance Test Examples/Scripts/FireBallSpawner.cs
@@ -13,20 +13,51 @@ namespace OptimizationExamples.PerformanceTestExamples
 		void Update()
 		{
 			if (Input.GetMouseButtonUp(0))
-				SpawnFireBalls(Random.Range(5, 25));
+				SpawnFireBalls_initial(Random.Range(5, 25));
 		}
 
-		public void SpawnFireBalls(int count)
+		/// <summary>
+		/// The first implemenation of spawn fire balls
+		/// </summary>
+		/// <param name="count"></param>
+		public void SpawnFireBalls_initial(int count)
 		{
 			for(int i = 0; i < count; i++)
 			{
 				Vector3 generatedPos = Random.onUnitSphere * 2.5f + _spawnPos.position;
-				//Instantiate(_fireBallPrefab, generatedPos, Quaternion.identity);
+				Instantiate(_fireBallPrefab, generatedPos, Quaternion.identity);
+			}
+		}
+
+		/// <summary>
+		/// The changes made by the intern collague
+		/// </summary>
+		/// <param name="count"></param>
+		public void SpawnFireBalls_intern_changes(int count)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				Vector3 generatedPos = Random.onUnitSphere * 2.5f + _spawnPos.position;
 				Destroy(Instantiate(_fireBallPrefab, generatedPos, Quaternion.identity), 3.0f);
 			}
 		}
 
-		// Ötletek:
+		/// <summary>
+		/// The changes made by the intern collague
+		/// </summary>
+		/// <param name="count"></param>
+		public void SpawnFireBalls_intern_changes_improved(int count)
+		{
+			GameObject parent = new GameObject();
+			for (int i = 0; i < count; i++)
+			{
+				Vector3 generatedPos = Random.onUnitSphere * 2.5f + _spawnPos.position;
+				Instantiate(_fireBallPrefab, generatedPos, Quaternion.identity, parent.transform);
+			}
+			Destroy(parent, 3.0f);
+		}
+
+		// Ötletek: Fontos a jelent GPU bound
 		// Az alap ami most van
 		// Destory mindenre egy fos megoldás jött egy új kolléga érkezik aki kijavítja a scriptet jóhiszeműen de hát szar lesz
 		// Destroy egy holder Go-ra
