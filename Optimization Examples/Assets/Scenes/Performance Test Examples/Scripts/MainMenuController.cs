8a2cefa4c699a70917c9d1a91286c47cefb28ef2
Menyus <menyus777@gmail.com>
2020-11-30 19:53:23 +0100

    Added Main Menu Controller script for UI handling

---
+++ Optimization Examples/Assets/Scenes/Performance Test Examples/Scripts/MainMenuController.cs
@@ -0,0 +1,37 @@
+﻿// Copyright 2020 by RedFox Interactive - All Rights Reserved
+// Unauthorized copying of this file, is strictly prohibited
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace OptimizationExamples.PerformanceTestExamples.UI
+{
+	/// <summary>
+	/// Controllers the Main Menu
+	/// </summary>
+	public class MainMenuController : MonoBehaviour
+	{
+		/// <summary>
+		/// The button which loads the infamous dragon level
+		/// </summary>
+		public Button LoadDragonLevelButton { get => _loadDragonLevelButton; }
+		[SerializeField]
+		Button _loadDragonLevelButton;
+
+		void Awake()
+		{
+			// Initializing the Load Dragon Button
+			_loadDragonLevelButton.onClick.AddListener(() => loadLevel("Performance Test Examples - Test Level"));
+		}
+
+		/// <summary>
+		/// Loads the specified scene
+		/// </summary>
+		/// <param name="levelName">The name of the scene that will be loaded</param>
+		void loadLevel(string levelName)
+		{
+			SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+		}
+	}
+}
