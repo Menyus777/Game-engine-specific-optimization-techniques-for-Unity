c088457768837f8865290e2362eb003f4203827d
Menyus <menyus777@gmail.com>
2020-12-02 21:44:38 +0100

    Fix method name

--- Optimization Examples/Assets/Tests/Performance/Performance Test Examples/DragonLevelPerformanceTests.cs
+++ Optimization Examples/Assets/Tests/Performance/Performance Test Examples/DragonLevelPerformanceTests.cs
@@ -14,8 +14,10 @@ namespace Tests.Performance.PerformanceTestExamples
         [UnitySetUp]
         public IEnumerator BeforeAll()
         {
+            // Loading the relevant scene
             SceneManager.LoadScene("Performance Test Examples - Dragon Level");
-            // Wait a frame for scene load
+            // Wait a frame for scene load - scenes are only loaded in the next frame
+            // loading was initiated
             yield return null;
         }
 
@@ -35,7 +37,7 @@ namespace Tests.Performance.PerformanceTestExamples
             {
                 for (int i = 0; i < 250; i++)
                 {
-                    fireBallSpawner.SpawnFireBalls_intern_changes(25);
+                    fireBallSpawner.SpawnFireBalls(25);
                     yield return userInputDelayYieldInstruction;
                 }
             }
@@ -45,8 +47,8 @@ namespace Tests.Performance.PerformanceTestExamples
             PerformanceTest.Active.CalculateStatisticalValues();
             var fpsResults = PerformanceTest.Active.SampleGroups.Find(s => s.Name == "FPS");
 
-            Assert.GreaterOrEqual(fpsResults.Median, 240,
-                "Violation of OG_117650: The median FPS should be higher than 60 frames per second.");
+            Assert.GreaterOrEqual(fpsResults.Median, 120,
+                "Violation of OG_117650: The median FPS should be higher than 120 frames per second.");
         }
 
     }
