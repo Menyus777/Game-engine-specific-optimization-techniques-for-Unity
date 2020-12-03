916ab474ffa1e947ccec713c9b17b13caab8ec26
Menyus <menyus777@gmail.com>
2020-12-03 16:09:08 +0100

    Initial commit of update manager script base

---
+++ Optimization Examples/Assets/Scenes/Update Manager/Scripts/Mover.cs
@@ -0,0 +1,36 @@
+﻿#define USE_UPDATE_MANAGER
+#undef USE_UPDATE_MANAGER
+
+using UnityEngine;
+
+namespace OptimizationExamples.UpdateManager
+{
+	public class Mover : MonoBehaviour
+	{
+
+		float _speed;
+
+		void Awake()
+		{
+			_speed = Random.Range(1.0f, 1.1f);
+		}
+
+#if !USE_UPDATE_MANAGER
+		void Update()
+		{
+			moveUpAndDown();
+		}
+#endif
+		public void UpdateManager_Update()
+		{
+			moveUpAndDown();
+		}
+
+		void moveUpAndDown()
+		{
+			var currPos = transform.position;
+			transform.position = new Vector3(currPos.x, Mathf.PingPong(Time.time * _speed, 10f), currPos.z);
+		}
+
+	}
+}
---
+++ Optimization Examples/Assets/Scenes/Update Manager/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace OptimizationExamples.UpdateManager
+{
+	public class Spawner : MonoBehaviour
+	{
+		[SerializeField]
+		GameObject _objectToSpawn;
+
+		void Start()
+		{
+			SpawnObjects(100);
+		}
+
+		/// <summary>
+		/// Will spawn count*count objects in the scene
+		/// </summary>
+		public void SpawnObjects(int count)
+		{
+			for(int i = 0; i < count * 2; i = i + 2)
+			{
+				for (int j = 0; j < count * 2; j = j + 2)
+				{
+					if (i == 2 && j == 2)
+						continue;
+					Instantiate(_objectToSpawn, new Vector3(i, 0f, j), Quaternion.identity);
+				}
+			}
+		}
+
+	}
+}
