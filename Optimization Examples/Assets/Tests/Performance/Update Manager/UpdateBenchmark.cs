90ec0a62d4ef7695beedbe820f8cba66293a9bdb
Menyus <menyus777@gmail.com>
2020-12-03 20:59:07 +0100

    Added benchmark

---
+++ Optimization Examples/Assets/Tests/Performance/Update Manager/UpdateBenchmark.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OptimizationExamples.UpdateManager;
+using System.Collections;
+using Unity.PerformanceTesting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests.Performance.UpdateManager
+{
+	public class UpdateBenchmark
+	{
+
+        [UnityTest, Performance]
+        public IEnumerator UpdateManager_Benchmark()
+        {
+            SceneManager.LoadScene("Update Manager");
+            Spawner.UseUpdateManager = true;
+
+            var sampleGroup = new SampleGroup("Load Time", SampleUnit.Millisecond);
+
+            // Wait a frame for scene load
+            yield return null;
+
+            
+
+            // Wait a frame for scene load
+            yield return null;
+
+            // Small idling before measurement starts
+            yield return new WaitForSecondsRealtime(2.0f);         
+        }
+
+    }
+}
