ab1bb429f668f7b63611ddbccad6d90c4da0fd13
Menyus <menyus777@gmail.com>
2020-12-06 15:39:17 +0100

    Added hierarchy optimization tests

--- Optimization Examples/Assets/Scenes/Hierarchy Optimization/Scripts/NPCMover.cs
+++ Optimization Examples/Assets/Scenes/Hierarchy Optimization/Scripts/NPCMover.cs
@@ -4,6 +4,11 @@ namespace OptimizationExamples.HierarchyOptimization
 {
 	public class NPCMover : MonoBehaviour
 	{
-		
+		float _speed = 2f;
+
+		void Update()
+		{
+			transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+		}
 	}
 }
---
+++ Optimization Examples/Assets/Tests/Performance/Hierarchy Optimization/HierarchyOptimizationBenchmark.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using Tests.Performance.Utilities;
+using Unity.PerformanceTesting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class HierarchyOptimizationBenchmark
+    {
+        const float _warmupTime = 5f;
+        const float _executionTime = 60f;
+
+        [UnityTest, Performance]
+        public IEnumerator Hierarchy_HeavyComposition_Benchmark()
+        {
+            // Loading the relevant scene
+            SceneManager.LoadScene("Hierarchy Optimization - Heavy Composition");
+            // Wait a frame for scene load - scenes are only loaded in the next frame
+            // loading was initiated
+            yield return null;
+
+            // Small warmup before measurement starts
+            yield return new WaitForSecondsRealtime(_warmupTime);
+
+            using (Measure.Frames().Scope("Frame Time"))
+            using (ScopedFPSMeasurement.StartFPSMeasurement("FPS"))
+            {
+                yield return new WaitForSecondsRealtime(_executionTime);
+            }
+        }
+
+        [UnityTest, Performance]
+        public IEnumerator Hierarchy_OptimizedComposition_Benchmark()
+        {
+            // Loading the relevant scene
+            SceneManager.LoadScene("Hierarchy Optimization - Optimized Composition");
+            // Wait a frame for scene load - scenes are only loaded in the next frame
+            // loading was initiated
+            yield return null;
+
+            // Small warmup before measurement starts
+            yield return new WaitForSecondsRealtime(_warmupTime);
+
+            using (Measure.Frames().Scope("Frame Time"))
+            using (ScopedFPSMeasurement.StartFPSMeasurement("FPS"))
+            {
+                yield return new WaitForSecondsRealtime(_executionTime);
+            }
+        }
+    }
+}
